4b91be8a2183f65252792f876795c5c142da624f
Oskeol Gjoni <oskeolgjoni@gmail.com>
2020-04-25 15:32:09 +0200

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
